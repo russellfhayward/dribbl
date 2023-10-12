93a3863723bc2efbb278cd423fec437332cf29f5
russell hayward <russdawg44@gmail.com>
2023-10-11 20:13:26 -0600

    added input for player name on canvas

--- src/Atoms/Canvas.tsx
+++ src/Atoms/Canvas.tsx
@@ -1,13 +1,40 @@
-import React, {useEffect, useRef } from "react";
+import React, {useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import useCanvas from "../Refs/useCanvas";
 
+const StyledCanvasOuter = styled.div`
+    width: 100%;
+    height: 100%;
+
+    position: relative;
+`
+
 const StyledCanvas = styled.canvas`
     display: inline-block;
-    border: 2px solid black;
     width: 80%;
     height: 80%;
+
+    border: 2px solid black;
 `;
+
+const PlayerNameInput = styled.input`
+    width: 8rem;
+    height: 1rem;
+
+    position: absolute;
+    top: 4%;
+    left: 11.5%;
+
+    white-space: nowrap;
+    overflow: hidden;  
+    text-overflow: ellipsis;
+
+    background-color: lightblue;
+
+    &::placeholder {
+        color: black;
+    }
+`
 
 interface CanvasComponentProps {
     lineWidth: number,
@@ -18,7 +45,7 @@ interface CanvasComponentProps {
 const CanvasComponent : React.FC< CanvasComponentProps & React.CanvasHTMLAttributes<HTMLCanvasElement>> = ({lineWidth, color, setClearCanvas,...props }) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const { clearCanvas } = useCanvas(canvasRef, lineWidth, color);
-
+    const [boardPlaceholder, setBoardPlaceholder] = useState('Player144234353553323535235235235235')
     useEffect(() => {
         if (canvasRef.current){
             setClearCanvas(() => clearCanvas);
@@ -36,7 +63,10 @@ const CanvasComponent : React.FC< CanvasComponentProps & React.CanvasHTMLAttribu
     }, [])
 
     return (
-        <StyledCanvas ref={canvasRef} {...props}></StyledCanvas>
+        <StyledCanvasOuter className="styled-canvas-outer">
+            <StyledCanvas ref={canvasRef} {...props}></StyledCanvas>
+            <PlayerNameInput placeholder={boardPlaceholder} />
+        </StyledCanvasOuter>
     )
 }
 
